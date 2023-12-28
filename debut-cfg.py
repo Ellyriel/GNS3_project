226dc73cc4ad804de1d9811130d8c77933b3da41
gaiiouch <ag.mauger@gmail.com>
2023-12-28 12:32:42 +0100

    MAJ de script pour création d'un fichier

--- debut-cfg.py
+++ debut-cfg.py
@@ -1,2 +1,7 @@
-def creation_fichier():
-    f = open("config.txt", "w")
+hostname = "R1"
+
+def creation_fichier(hostname):
+    name = "config_"+ hostname + ".cfg"
+    f = open(name,"w")
+
+creation_fichier(hostname)
