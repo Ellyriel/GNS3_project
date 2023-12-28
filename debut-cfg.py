c5416b8898cf5d840abe19e1eac7b261b8a1646e
gaiiouch <ag.mauger@gmail.com>
2023-12-28 13:06:14 +0100

    Récupération de l'heure et de la date actuelle. FONCTIONNE. Sera utilisée lors de la création du fichier.

--- debut-cfg.py
+++ debut-cfg.py
@@ -1,7 +1,12 @@
+import datetime
+
 hostname = "R1"
 
 def creation_fichier(hostname):
     name = "config_"+ hostname + ".cfg"
     f = open(name,"w")
 
-creation_fichier(hostname)
+def recuperer_date():
+    x = datetime.datetime.now()
+    date = x.strftime("%H:%M:%S UTC %a %b %d %Y")
+    return date
