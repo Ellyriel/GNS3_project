6d19aa88f836aeccd5b983c09e5c101070c7c1d9
gaiiouch <ag.mauger@gmail.com>
2023-12-28 14:55:43 +0100

    Amélioration avec ajout d'une fonction écriture et de création de texte, plutôt que de créer un texte puis l'écrire en boucle

--- debut-cfg.py
+++ debut-cfg.py
@@ -8,40 +8,43 @@ def creation_fichier(hostname):
     f = open(name,"w")
     return f
 
+def ecriture_fichier(file,text):
+    file.write(text)
+
 def recuperer_date():
     x = datetime.datetime.now()
     date = x.strftime("%H:%M:%S UTC %a %b %d %Y")
     return date
 
-def ecriture_debut(hostname, ip_version):
-    fichier_config = creation_fichier(hostname)
-    fichier_config.write("!\n"*3)
-    fichier_config.write("! Last configuration change at " + recuperer_date() + "\n")
+def creation_texte(hostname, ip_version):
+    texte = ""
 
-    texte = "!\nversion 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\n!\n"
-    fichier_config.write(texte)
-    fichier_config.write("hostname "+ hostname + "\n!\n")
+    texte += "!\n"*3
+    texte += "! Last configuration change at " + recuperer_date() + "\n"
 
-    texte = "boot-start-marker\nboot-end-marker\n"
-    fichier_config.write(texte + "!\n"*3)
+    texte += "!\nversion 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\n!\n"
+    texte += "hostname "+ hostname + "\n!\n"
 
-    texte = "no aaa new-model\nno ip icmp rate-limit unreachable\nip cef\n"
-    fichier_config.write(texte + "!\n"*6)
+    texte += "boot-start-marker\nboot-end-marker\n" + "!\n"*3
+
+    texte += "no aaa new-model\nno ip icmp rate-limit unreachable\nip cef\n" + "!\n"*6
 
     if ip_version == 6 :
-        texte = "no ip domain lookup\nipv6 unicast-routing\nipv6 cef\n"
+        texte += "no ip domain lookup\nipv6 unicast-routing\nipv6 cef\n"
     elif ip_version == 4 :
-        texte = "JE NE SAIS PAS POUR LE MOMENT, A RECHERCHER\n"
+        texte += "JE NE SAIS PAS POUR LE MOMENT, A RECHERCHER\n"
     else :
         print("ERROR : improper IP version")
         return
     
-    fichier_config.write(texte + "!\n"*2)
+    texte += "!\n"*2
 
-    texte = "multilink bundle-name authenticated\n"
-    fichier_config.write(texte + "!\n"*9)
+    texte += "multilink bundle-name authenticated\n" + "!\n"*9
+    texte += "ip tcp synwait-time 5\n" + "!\n"*12
+    
+    return texte
 
-    texte = "ip tcp synwait-time 5\n"
-    fichier_config.write(texte + "!\n"*12)
 
-ecriture_debut(hostname, ip_version)
+fichier_config = creation_fichier(hostname)
+texte_config = creation_texte(hostname, ip_version)
+ecriture_fichier(fichier_config,texte_config)
